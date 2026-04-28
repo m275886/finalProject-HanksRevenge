6be5eb1751de9bdbfc16cc46974c33154b79c103
theranger815 <somasleep1@gmail.com>
2026-04-28 06:45:31 -0400

    Update: Removed unnecessary function address resolutions from HankInitialHost.c

--- host/HankInitialHost.c
+++ host/HankInitialHost.c
@@ -17,9 +17,8 @@ typedef VOID (*HankStopFunction)(VOID);
 int wmain(int argc, wchar_t* argv[])
 {
 	HMODULE dllModule = NULL;
-	HankInitializeFunction hankInitialize = NULL;
-	HankStartFunction hankStart = NULL;
-	HankStopFunction hankStop = NULL;
+	
+	
 	PCWSTR dllPath = L"Hanks_Revenge.dll";
 	PCWSTR c2Host = L"127.0.0.1";
 	PCWSTR c2Port = L"9001";
@@ -49,37 +48,14 @@ int wmain(int argc, wchar_t* argv[])
 		return 1;
 	}
 
-	hankInitialize = (HankInitializeFunction)GetProcAddress(
-		dllModule,
-		"HankInitialize"
-	);
-	hankStart = (HankStartFunction)GetProcAddress(dllModule, "HankStart");
-	hankStop = (HankStopFunction)GetProcAddress(dllModule, "HankStop");
-	if (hankInitialize == NULL || hankStart == NULL || hankStop == NULL)
-	{
-		wprintf(L"[!] Failed to resolve one or more exports.\n");
-		FreeLibrary(dllModule);
-		return 1;
-	}
+	
 
-	/*/if (!hankInitialize(c2Host, c2Port))
-	{
-		wprintf(L"[!] HankInitialize failed.\n");
-		FreeLibrary(dllModule);
-		return 1;
-	}
-
-	if (!hankStart())
-	{
-		wprintf(L"[!] HankStart failed.\n");
-		FreeLibrary(dllModule);
-		return 1;
-	}*/
+	
 
 	wprintf(L"[*] Polling started. Press Enter to stop the host.\n");
 	(void)getwchar();
 
-	hankStop();
+
 	FreeLibrary(dllModule);
 	wprintf(L"[*] Host stopped.\n");
 
